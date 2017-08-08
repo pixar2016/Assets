736c4a369e1c7c9698fdfb5435997a111b7a31b9
pixar2016 <360970742@qq.com>
2017-08-08 20:27:19 +0800

    增加按钮响应

--- Scripts/UI/BattleMainPanel.cs
+++ Scripts/UI/BattleMainPanel.cs
@@ -4,9 +4,29 @@ using UnityEngine.UI;
 
 public class BattleMainPanel : UIComponent
 {
+    public GameObject BtnMagic1;
+    public GameObject BtnMagic2;
     public override void OnInit(object[] data)
     {
+        BtnMagic1 = transform.Find("LeftDown/Magic1").gameObject;
+        BtnMagic2 = transform.Find("LeftDown/Magic2").gameObject;
         UIEventSystem.Instance.Register("ShowBuildingProcess", ShowBuildingProcess);
+    }
+
+    public void SetEventListener()
+    {
+        UIEventListener.Get(BtnMagic1).onClick = OnBtnMagic1Click;
+        UIEventListener.Get(BtnMagic2).onClick = OnBtnMagic2Click;
+    }
+
+    public void OnBtnMagic1Click(GameObject go)
+    {
+
+    }
+
+    public void OnBtnMagic2Click(GameObject go)
+    {
+
     }
 
     public void ShowBuildingProcess(object[] param)
